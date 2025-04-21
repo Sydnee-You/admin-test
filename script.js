a3af9c2ea20571792268a1972bd3265f9fdda09f
Syd <sydinhi@gmail.com>
2025-04-20 22:02:23 -1000

    added checking for admin code

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 var test = document.getElementById("test");
+let currentCode ="tf12";
+var adminCode = document.getElementById("admin-code");
+var button = document.getElementById("button1");
 
 test.addEventListener("click", function() {
   test.innerHTML = "testing 123";
 })
+
+function adminCheck() {
+  if(adminCode.value == currentCode) {
+    document.getElementById("check").innerHTML ="correct";
+  } else {
+    document.getElementById("check").innerHTML ="incorrect";
+    console.log(currentCode);
+    console.log(adminCode.value);
+
+
+  }
+}
+
+button.addEventListener("click", adminCheck);
